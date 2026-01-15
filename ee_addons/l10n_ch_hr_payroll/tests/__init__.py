9fcc911f13194ee399bfded257c7a0683576c74b
Sotatek-TienTa <tien.ta@sotatek.com>
2026-01-15 14:48:37 +0700

    update

--- ee_addons/l10n_ch_hr_payroll/tests/__init__.py
+++
@@ -1,6 +0,0 @@
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
-
-from . import test_contract_template_whitelist
-from . import swissdec_common
-from . import test_swissdec_cases
-from . import test_swiss_warnings
